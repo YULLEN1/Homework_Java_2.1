3e6f456f98381be54b8c80e5811a533b6a7da397
yulialener <lener.yu@yandex.ru>
2024-06-16 12:02:55 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int price = 13676;
+
+        System.out.println("Количество начисленных миль за билет = " + price / 20);
+
+
+    }
+}
